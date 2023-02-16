a53524f2cdbd9ebc3e1583e6a51932144a2ed94e
Brandon Shihabi <brandon.shihabi@gmail.com>
2023-02-15 22:02:37 -0800

    Hot fix to prevent further merge errors

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
-import Button from '@mui/material/Button'
 import { ProFlow } from "./proflow/ProFlow";
-import { ApiError } from "./proflow/core/ApiError";
 import { BACKEND_PORT } from "./env";
 import './MainPage.css';
-import { Column, Profile, NoteCard, closeAddNotesIcon, addNotes, PopupBox } from './MainPage'; 
-import { deepStrictEqual } from 'assert';
+import { MainPage } from "./MainPage";
 
-class AppState {
+export class AppState {
 	private jwt?: string = undefined;
 
 	get client() {
@@ -46,64 +43,8 @@ class AppState {
 
 const App = () => {
 	const state = new AppState();
-	const login_email = "user@gmail.com";
-	const login_password = "test";
-	return (
-	<body>
-		<div className = "Main-Page">
-			<nav>
-				<img src="LOGO-HERE" className = "logo"></img>
-				<ul>
-					<li>
-						<Button variant="contained" className = "Button-Design"onClick={async () => {
-						try {
-							const res = await state.client.auth.authSignup({ email: login_email, password: login_password });
-							state.authorize(res.jwt, res.expire_sec);
-						} catch (e) {
-							if (e instanceof ApiError) {
-								console.log("Request failed (" + e.status + ") error: " + e.body.message);
-							}
-						}
-						}}>Signup</Button>
-					</li>
-
-					<li>
-						<Button variant="contained" onClick={async () => {
-							const res = await state.client.auth.authLogin({ email: login_email, password: login_password });
-							console.log("Logged in " + res.jwt)
-							state.authorize(res.jwt, res.expire_sec);
-						}}>Login</Button>
-						
-					</li>
-					<li>
-						<Button variant="contained" onClick={async () => {
-							const res = await state.client.user.getUserProjects();
-							console.log("Get projects " + res.project_guids)
-						}}>Get Projects</Button>
-					</li>
-
-
-				</ul>
-				<Profile UserName='[NAME HERE]'></Profile>
-
-			</nav>
-			<PopupBox></PopupBox>
-		
-			<div className = "wrapper">
-				<Column title="Backing">
-					<div>
-						<NoteCard title="Title" description="description..." time="time"></NoteCard>
-					</div>
-				</Column>
-				<Column title="Design"></Column>
-				<Column title="To Do"></Column>
-				<Column title="Doing"></Column>
-		
-			</div>
-		</div>
-	</body>
-	
-	);
+	// Change this when testing!
+	return <MainPage state={state}></MainPage>;
 }
 
 export default App;
