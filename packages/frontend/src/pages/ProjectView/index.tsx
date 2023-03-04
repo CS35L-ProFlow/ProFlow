d81a3c421c4f4e6dd85d397344c289c290c4cea3
davidjin9294 <zjin25@ucla.edu>
2023-03-03 22:22:34 -0800

    Implemented drag and drop on individual note card
    frontend/src/pages/ProjectView/index.tsx: Implemented the drag and drop
        functions. It will also calculate where the cursor is to determine the
        exact location of where to drop the card
    frontend/src/pages/ProjectView/index.css: Implemented related css for drag
        and drop

--- packages/frontend/src/pages/ProjectView/index.tsx
+++ packages/frontend/src/pages/ProjectView/index.tsx
@@ -5,8 +5,11 @@ import Pages from "../../pages";
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { GetProjectResponse } from '../../proflow';
+import { createTheme, CssBaseline, IconButton, InputBase, makeStyles, Paper, Typography } from '@mui/material';
+import { Title } from '@mui/icons-material';
 
 //This file contains the packground, drop down menu, and cards.
+
 
 //New column
 export interface ColumnProps {
@@ -20,11 +23,12 @@ export function Column(props: ColumnProps) {
 			<p>{props.title}</p>
 			<hr></hr>
 		</div>
-		{props.children}
 		<div className="add-buttom">
 			<Button id="add-note-button" onClick={addNotesButton}>Add new Notes</Button>
 		</div>
-	</li>;
+		{props.children}
+		
+	</li>
 }
 
 //User Profile
@@ -75,20 +79,25 @@ export interface NoteProps {
 	children?: React.ReactNode,
 }
 export function NoteCard(props: NoteProps) {
-	return <div className="note-card">
+	return <div className="note-card" draggable="true">
 		<p>{props.title}</p>
 		<span>{props.description}</span>
 		<div className="bottom-content">
 			<span>{props.time}</span>
-			<div className='settings'>
+			{/* <div className='settings'>
 				<i>Setting</i>
 				<ul className="menu">
 					<li>Edit</li>
 					<li>Delete</li>
 				</ul>
-			</div>
+			</div> */}
 		</div>
 	</div>
+	
+}
+
+export function DeleteNode(){
+	
 }
 
 //Popup Box:
@@ -138,21 +147,48 @@ export function SidePanel(props:PanelProps){
 				</button>
 
 				<div className='main'>
-					<div className = "wrapper">
-						<Column title="Backing">
-							<div>
-								<NoteCard title="Title" description="description..." time="time"></NoteCard>
-							</div>
-						</Column>
-						<Column title="Design"></Column>
-						<Column title="To Do"></Column>
-						<Column title="Doing"></Column>
+				<div className="wrapper">
+					<Column title="Backing">
+						
+						<div className='card-layout'>
+							<NoteCard title="A" description="description..." time="time"></NoteCard>
 
-					</div>
+							<NoteCard title="B" description="description..." time="time"></NoteCard>
+
+							<NoteCard title="C" description="description..." time="time"></NoteCard>
+						</div>
+					</Column>
+					<Column title="Design">
+						<div>
+							<NoteCard title="D" description="description..." time="time"></NoteCard>
+
+							<NoteCard title="E" description="description..." time="time"></NoteCard>
+
+							<NoteCard title="F" description="description..." time="time"></NoteCard>
+						</div>
+					</Column>
+					<Column title="To Do">
+						<div>
+							<NoteCard title="G" description="description..." time="time"></NoteCard>
+
+							<NoteCard title="H" description="description..." time="time"></NoteCard>
+
+							<NoteCard title="I" description="description..." time="time"></NoteCard>
+						</div>  
+					</Column>
+					<Column title="Doing">
+					</Column>
+
+
+				</div> 
 				</div>
 			</div>
 }
 //Helper Functions Below:
+//Append card
+export function AppendCard(columnId:string ,NoteCard:HTMLElement){
+	//TODO: figure out how to pass in column id. 
+}
 
 //Show User Menu
 export function toggleMenu() {
@@ -181,13 +217,14 @@ export function showNotes() {
 	});
 }
 
+
 //Add new notes
 export function addNotes() {
 	let addButton = document.getElementById("save-note-button")
 	let titleTag = document.querySelector("input")
 	let descriptionTag = document.querySelector("textarea")
-	let noteTitle = titleTag?.value;
-	let noteDescription = descriptionTag?.value;
+	let noteTitle = titleTag!.value;
+	let noteDescription = descriptionTag!.value;
 	const monthArray = ["January", "February", "March", "April", "May", "June", "July",
 		"August", "September", "October", "November", "December"];
 
@@ -204,6 +241,7 @@ export function addNotes() {
 		}
 		notes.push(noteInfo);
 		localStorage.setItem("notes", JSON.stringify(notes));
+		<NoteCard title={noteTitle} description={noteDescription} time = {`${month} ${day} ${year}`}></NoteCard>
 		closeAddNotesIcon();
 	}
 }
@@ -216,6 +254,9 @@ export function toggleSidePanel(){
 	return sidePanel!.classList.toggle("open-side-panel")
 	
 }
+
+
+
 
 export interface ProjectViewProps {
 	session: Session | undefined,
@@ -249,13 +290,13 @@ export default function ProjectView(props: ProjectViewProps) {
 		fetchProjectInfo();
 	}, [])
 	const title = projInfo?.name;
+	DragAndDrop();
 
 	return (
 		<body>
 			<div className="Main-Page">
 				<nav>
 					<img src="LOGO-HERE" className="logo"></img>
-					<h1>{title}</h1>
 					<ul>
 						<li>
 							<Link to={Pages.SIGNUP}>
@@ -266,11 +307,6 @@ export default function ProjectView(props: ProjectViewProps) {
 						</li>
 
 						<li>
-							{/* <Button variant="contained" onClick={async () => {
-								const res = await state.client.auth.authLogin({ email: login_email, password: login_password });
-								console.log("Logged in " + res.jwt)
-								state.authorize(res.jwt, res.expire_sec);
-							}}>Login</Button> */}
 							<Link to={Pages.LOGIN}>
 								<Button variant="contained" className="Button-Design">
 									LOG IN
@@ -292,19 +328,73 @@ export default function ProjectView(props: ProjectViewProps) {
 				</nav>
 				<PopupBox></PopupBox>
 
-				<div className="wrapper">
-					<Column title="Backing">
-						<div>
-							<NoteCard title="Title" description="description..." time="time"></NoteCard>
-						</div>
-					</Column>
-					<Column title="Design"></Column>
-					<Column title="To Do"></Column>
-					<Column title="Doing"></Column>
-
-				</div>
+				<SidePanel ProjectTitle1='Project 1' ProjectTitle2='Project 2'></SidePanel>
 			</div>
 		</body>
 
 	);
+}
+
+//Drag and Drop
+
+//Drag and Drop
+
+
+function DragAndDrop(){
+const list_items = document.querySelectorAll(".note-card") as NodeListOf<HTMLElement>;
+const lists = document.querySelectorAll(".note") as NodeListOf<HTMLElement>;
+//item we are dragging
+let draggedItem:any = null;
+for(let i = 0; i< list_items.length; i++){
+	const item = list_items[i];
+
+	item.addEventListener('dragstart', function(){
+		draggedItem = item;
+		item.classList.add('dragging')
+		setTimeout(function(){
+			draggedItem.style.display = 'none';
+		},0)
+		
+	});
+	
+	item.addEventListener('dragend', function(){
+		item.classList.remove('dragging')
+		setTimeout(function(){
+			draggedItem.style.display = 'block';
+			draggedItem = null;
+		}, 0);
+	});
+}
+	for(let j = 0; j<lists.length; j++){
+		const list = lists[j]
+		list.addEventListener('dragover', function(e){
+			e.preventDefault();
+			const afterElement = getDragAfterElement(list, e.clientY)
+			console.log(afterElement)
+			if(afterElement == null){
+				list.append(draggedItem);
+			}else{
+				list.insertBefore(draggedItem, afterElement)
+			}
+
+		});
+	}
+}
+
+
+
+
+function getDragAfterElement(list:any, y:number){
+	const draggableElements = [...list.querySelectorAll('.note-card:not(.dragging)')]
+	return draggableElements.reduce((closest, child) =>{
+		const box = child.getBoundingClientRect()
+		const offset = y - box.top - box.height / 2
+		if(offset < 0 && offset > closest.offset){
+			return { offset: offset, element: child}
+		}else {
+			return closest
+
+			}
+		
+	},{ offset: Number.NEGATIVE_INFINITY }).element
 }
